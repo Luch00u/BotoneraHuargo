c30bff1b0be0f0cc3269b3c8a5af60712fd60327
Luciano.Jaime <jaimeluciano838@gmail.com>
2022-09-01 23:49:58 -0300

    pruebaa

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const btn7 = document.getElementById('btn7');
 const ahh = new Audio();
-ahh.src = "../audio/ahhDody.mp3";
+ahh.src = "./audio/ahhDody.mp3";
 btn7.addEventListener('click', () => {
    ahh.play();
 });
